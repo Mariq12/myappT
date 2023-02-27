82f17a747ddce90851847c2151b16f5d55ea5446
Mariq12 <fmlabanda@utpl.edu.ec>
2023-02-27 11:28:31 -0500

    nuevo cambio

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Hola Mari',
+        title: 'Bienvenido a la app Mari',
         home: HomeScreen());
   }
 }
