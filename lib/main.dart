3595c688b8105df17ad8150c3b24d004ae213999
Mari <101030215+Mariq12@users.noreply.github.com>
2023-03-07 14:49:41 -0500

    prueb
    jenkins

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Bienvenido a la app Mariqlp',
+        title: 'Bienvenido a la app Mariqlp jenkins',
         home: HomeScreen());
   }
 }
